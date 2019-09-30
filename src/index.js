01845daf1b9f7286066f385481f4b93eccc99423
Solej77 <ing.josgalo@gmail.com>
2019-09-29 22:01:20 -0500

    Reto 05 - Successfull

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
+// const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
+let counterPage = 0;
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      const characters = response.results;
+async function dataMaker(api) {
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    const characters = await data.results;
+          
+    localStorage.setItem('next_fecth', data.info.next);
+    if (data.info.next) {
+      debugger
       let output = characters.map(character => {
         return `
       <article class="Card">
@@ -19,11 +25,44 @@ const getData = api => {
       newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+    } else {
+      debugger
+      let newItem = document.createElement('section');
+      newItem.classList.add('Items');
+      newItem.innerHTML = 'Ya no hay personajes ...';
+      $app.appendChild(newItem);
+      intersectionObserver.unobserve($observe);
+    }
+    
+  } catch (error) {
+    let newItem = document.createElement('section');
+    newItem.classList.add('Items');
+    newItem.innerHTML = 'OOOOOOOOOOOOPS!!!, OCURRIO UN ERROR';
+    $app.appendChild(newItem);
+  }
 }
 
-const loadData = () => {
+const getData = api => {
+  const next_fecth = localStorage.getItem('next_fecth');
+  if (next_fecth) {
+    let api
+    if (counterPage === 0) {
+      api = API;
+      localStorage.clear()
+    } else {
+      api = next_fecth;
+    }
+
+    dataMaker(api)
+    counterPage++;
+  } else {
+    dataMaker(API);
+    counterPage++;
+  }
+
+}
+
+const loadData = async function() {
   getData(API);
 }
 
